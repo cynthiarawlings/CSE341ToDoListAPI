0081aa7294b820abe39e032971dcf9d8d98d6afb
hyruleismyrule <hyruleismyrule@gmail.com>
2022-12-08 18:38:51 -0500

    user can view their lists

--- controllers/user.js
+++ controllers/user.js
@@ -3,25 +3,31 @@
 
 
 const mongodb = require('../connections/index');
+const ObjectId = require('mongodb').ObjectId;
 
 
 const getUserLists = async (req, res) => {
+    console.log("get list function");
     try {
-        if (!authZeroUserJson.sub) {
-            res.status(400).send({ message: 'No 0auth id supplied.' });
+        const userId = new ObjectId(req.params.id);
+        if (!userId) {
+            res.status(400).send({ message: 'No Id supplied.' });
             return;
         }
-        await mongodb.getDb().db('CSE341ToDoListAPI').collection('users').find({ identifier: authZeroUserJson.sub })
+        await mongodb.getDb().db('CSE341ToDoListAPI').collection('users').find({ _id: userId }).toArray()
             .then((result) => {
+                console.log("sending data");
                 res.setHeader('Content-Type', 'application/json');
                 res.status(200).json(result);
             })
             .catch((err) => {
+                console.log("Error")
                 res.status(500).send({
                     message: err.message || 'Some error occurred while retrieving the user lists.'
                 });
             });
     } catch (err) {
+        console.log("Error #2")
         res.status(500).json(err);
     }
 };
--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,8 @@ const router = express.Router();
 const userController = require('../controllers/user.js');
 const loadUser = require("../middleware/loadUser");
 
-router.get('/', userController.getUserLists);
+// router.get('/', userController.getUserLists);
+router.get('/:id',userController.getUserLists);
 router.use([loadUser]);
 
 
