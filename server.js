ee1d6d58ff5e147ee8fc76da2e6d0d664eef3528
Erendira Can Radichel <59750812+ErendiraCan@users.noreply.github.com>
2022-12-09 23:20:27 -0600

    Updating

--- server.js
+++ server.js
@@ -7,16 +7,17 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const mongodb = require('./connections/index');
 const swaggerUi = require('swagger-ui-express');
-const cors = require('cors');
-const cookieParser = require("cookie-parser");
+//const cors = require('cors');
+//const cookieParser = require("cookie-parser");
 // const swaggerDocumentInstructions = require('./swagger');
 const swaggerDocument = require('./documentation/swagger-example.json');
+const { auth, requiresAuth } = require('express-openid-connect');
 
 app
-    .use(cors())
+    //.use(cors())
     .use(bodyParser.json())
-    .use(express.urlencoded({ extended: true }))
-    .use(cookieParser())
+    //.use(express.urlencoded({ extended: true }))
+    //.use(cookieParser())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         next();
@@ -35,4 +36,25 @@ mongodb.initDb((err, mongodb) => {
     app.listen(port);
     console.log(`Connected to DB and listening on ${port}`);
   }
+});
+
+//Auth Autentification
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
